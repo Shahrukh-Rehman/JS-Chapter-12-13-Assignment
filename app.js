a2689e2dcd76a2d876054d5d64ad66618041ab60
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-12 18:58:52 +0500

    Question 1 to 7

---
+++ app.js
@@ -0,0 +1,87 @@
+//1
+// var input =Number(prompt("Write a character or number for check").charCodeAt())
+
+// if(input >= 65 && input<=90){
+//     alert("Your Character is Uppercase")
+// }
+// else if(input >= 97 && input<=122){
+//     alert("Your Character is Lowercase")
+// }
+// else if(input >= 48 && input<=57){
+//     alert("You type a Number")
+// }
+// else(alert("Sorry! Type only number or alphabets"))
+//2
+// var firstInteger = Number(prompt("Wrie first integer: "))
+// var secondInteger = Number(prompt("Wrie second integer: "))
+// if(firstInteger>secondInteger){
+//     alert(firstInteger + " is greater than "+ secondInteger)
+// }
+// else if(secondInteger>firstInteger){
+//     alert(secondInteger + " is greater than "+ firstInteger)
+// }
+// else if(secondInteger === firstInteger){
+//     alert(secondInteger + " is equal to "+ firstInteger)
+// }
+
+//3
+// var firstInteger = Number(prompt("Type an integar: "))
+
+// if(firstInteger>0){
+//     alert("Your input is the postive value")
+// }
+// else if(firstInteger<0){
+//     alert("Your input is the negative value")
+// }
+// else if(firstInteger === 0){
+//     alert("Your input is the zero")
+// }
+
+//4
+// var input=prompt("Typa a character : ").toLowerCase()
+// if(input==="a" || input==="e" || input==="i" || input==="o" || input==="u"){
+//     alert("True, its a vowel")
+// }
+// else (
+//     alert("False, No its not a vowel")
+// )
+//5
+// var password="123098qwe"
+// var givenPassword = prompt("Please enter your password: ")
+// if(givenPassword===password){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else(alert("Incorrect password"))
+//6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";       //INCORRECT ONE
+// else
+// greeting = "Good evening";
+// }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {                //correct one
+// greeting = "Good day";
+// }
+// else(greeting = "Good evening")
+
+//7
+// var time = Number(prompt("What's time is it, type in 24hrs format: "))
+
+// if(time >= 0000 && time < 1200){
+//     alert("Good Morning!")
+// }
+// else if(time >= 1200 && time < 1700){
+//     alert("Good Afternoon!")
+// }
+// else if(time >= 1700 && time < 2100){
+//     alert("Good Evening!")
+// }
+// else if(time >= 2100 && time < 2359){
+//     alert("Good Night!")
+// }
+
+
